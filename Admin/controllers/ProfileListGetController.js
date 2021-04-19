f6977144fa311d1e7559e31088a11aeee5a897e9
kaksity <daudapon2@gmail.com>
2021-04-19 07:54:41 -0400

    Change the Middle Name to Return Empty than Null

--- Admin/controllers/ProfileListGetController.js
+++ Admin/controllers/ProfileListGetController.js
@@ -7,10 +7,10 @@ module.exports = async(req,res) => {
         const Type = req.query.type;
 	const Page = req.query.page;
 	const Size = req.query.size;
-	
+
         //Some Simple Validation
         QueryParams = {};
-        
+
         if(Type == undefined || Page == undefined || Size == undefined){
             return res.status(400).json({
                 success:false,
@@ -39,9 +39,9 @@ module.exports = async(req,res) => {
             	message:'Maximun Allowable Page Size is 1000'
        	});
         }
-              
+
         const OffSet = (Page - 1 ) * Size;
-        
+
         let result = await Profile.findAndCountAll({
             attributes:['id','firstname','middlename','lastname','gender','phone','highest_education_level','lga_id','lga.name'],
             where:{
@@ -60,28 +60,28 @@ module.exports = async(req,res) => {
 
 
 	let TotalNumberOfRecord = result.count;
-	
+
 	let TotalNumberOfPages =  Math.floor(TotalNumberOfRecord / Size);
-	
+
 	if(TotalNumberOfRecord % Size != 0){
 		TotalNumberOfPages=TotalNumberOfPages+1;
 	}
-	
+
 	let DataResult = result.rows;
-	
+
         let data = [];
-        
+
         DataResult.forEach(element => {
             data.push({
                 id:element.id,
-                full_name:`${element.firstname} ${element.lastname} ${element.middlename}`,
+                full_name:`${element.firstname} ${element.lastname} ${(element.middlename) != null ? element.middlename : ''}`,
                 gender:element.gender == null?'':element.gender,
                 phone_number:element.phone == null?'':element.phone,
                 highest_qualification:element.highest_education_level == null?'':element.highest_education_level,
                 lga:element.lga_id == null?'':element.lga.name,
             });
         })
-	
+
         return res.status(200).json({
             success:true,
             message:'Retrieved Profile Successfully',
@@ -91,12 +91,12 @@ module.exports = async(req,res) => {
                 total_number_pages:TotalNumberOfPages,
                 items_return:data.length,
             }
-        })       
+        })
     } catch (e) {
         console.log(e);
         return res.status(500).json({
             success:false,
             message:'Something went wrong. Try again'
         });
-    }   
+    }
 }
