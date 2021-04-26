e5b3c105fb46c99b82dd9df8b672eb9201b715de
coldflame <daudapona2@gmail.com>
2021-04-26 17:31:51 +0100

    Refractored Some Code On the Server

--- Users/controllers/RegisterController.js
+++ Users/controllers/RegisterController.js
@@ -5,7 +5,7 @@ const Profile = require('../models/Profile');
 const bcryptjs = require('bcryptjs');
 const db = require('../config/db');
 
-module.exports = async(req,res)=>{
+module.exports = async(req, res) => {
     try {
         let FirstName = req.body.first_name;
         let LastName = req.body.last_name;
@@ -15,64 +15,63 @@ module.exports = async(req,res)=>{
         let ConfirmPassword = req.body.confirm_password;
 
 
-        if(validator.isEmpty(FirstName)){
+        if (validator.isEmpty(FirstName)) {
             return res.status(400).json({
-                success:false,
-                message:'First Name is required'
+                success: false,
+                message: 'First Name is required'
             });
-        }else if(validator.isEmpty(LastName)){
+        } else if (validator.isEmpty(LastName)) {
             return res.status(400).json({
-                success:false,
-                message:'Last Name is required'
+                success: false,
+                message: 'Last Name is required'
             });
-        }else if(validator.isEmpty(EmailAddress)){
+        } else if (validator.isEmpty(EmailAddress)) {
             return res.status(400).json({
-                success:false,
-                message:''
+                success: false,
+                message: ''
             });
-        }else if(validator.isEmpty(PhoneNumber)){
+        } else if (validator.isEmpty(PhoneNumber)) {
             return res.status(400).json({
-                success:false,
-                message:'Phone Number is required'
+                success: false,
+                message: 'Phone Number is required'
             });
-        }else if(validator.isEmpty(Password)){
+        } else if (validator.isEmpty(Password)) {
             return res.status(400).json({
-                success:false,
-                message:'Password is required'
+                success: false,
+                message: 'Password is required'
             });
-        }else if(validator.isEmpty(ConfirmPassword)){
+        } else if (validator.isEmpty(ConfirmPassword)) {
             return res.status(400).json({
-                success:false,
-                message:'Confirm Password is required'
+                success: false,
+                message: 'Confirm Password is required'
             });
-        }
-        else if(validator.isEmail(EmailAddress) == false){
+        } else if (validator.isEmail(EmailAddress) == false) {
             return res.status(400).json({
-                success:false,
-                message:'Email Address is invalid'
+                success: false,
+                message: 'Email Address is invalid'
             });
         }
         //Check if the Length of the Phone Number is 11
-        if(PhoneNumber.length != 11){
+        if (PhoneNumber.length != 11) {
             return res.status(400).json({
-                success:false,
-                message:'Phone Number must be 11 digits'
+                success: false,
+                message: 'Phone Number must be 11 digits'
             });
         }
 
         //Check if the Length of the Password is 8
-        if(Password.length < 8){
+        if (Password.length < 8) {
             return res.status(400).json({
-                success:false,
-                message:'Password length must be 8 or more characters'
+                success: false,
+                message: 'Password length must be 8 or more characters'
             });
         }
 
         //Check if the password matches the confirm password
-        if(Password != ConfirmPassword){
+        if (Password != ConfirmPassword) {
             return res.status(400).json({
-                success:false,
-                message:'Password must match Confirm Password'
+                success: false,
+                message: 'Password must match Confirm Password'
             });
         }
         let transaction = await db.transaction();
@@ -81,81 +80,80 @@ module.exports = async(req,res)=>{
         //Check if the email Address has already been taken
         try {
             let result = await UserInfo.findOne({
-                where:{
-                    email_address:EmailAddress
+                where: {
+                    email_address: EmailAddress
                 }
             });
 
-            if(result){
+            if (result) {
                 return res.status(400).json({
-                    success:false,
-                    message:'This E-mail Address has already been taken'
+                    success: false,
+                    message: 'This E-mail Address has already been taken'
                 });
             }
 
             // Since the Email Address has not been taken. Hash the Password and been the process to insert data into db
 
-            let HashedPassword = await bcryptjs.hash(Password,10);
+            let HashedPassword = await bcryptjs.hash(Password, 10);
 
             try {
 
                 let ProfileResult = await Profile.create({
-                    firstname:FirstName,
-                    lastname:LastName,
-                    phone:PhoneNumber,
-                    educated:false,
-                    profile_completed:false,
-                },{
-                    transaction:transaction
+                    firstname: FirstName,
+                    lastname: LastName,
+                    phone: PhoneNumber,
+                    educated: false,
+                    profile_completed: false,
+                }, {
+                    transaction: transaction
                 });
 
                 let UserResult = await UserInfo.create({
-                    enabled:true,
-                    firstname:FirstName,
-                    is_using2fa:false,
-                    lastname:LastName,
-                    password:HashedPassword,
-                    phone:PhoneNumber,
-                    email_address:EmailAddress,
-                    profile_id:ProfileResult.id
-                },{
-                    transaction:transaction
+                    enabled: true,
+                    firstname: FirstName,
+                    is_using2fa: false,
+                    lastname: LastName,
+                    password: HashedPassword,
+                    phone: PhoneNumber,
+                    email_address: EmailAddress,
+                    profile_id: ProfileResult.id
+                }, {
+                    transaction: transaction
                 });
 
                 let UserRoleResult = await UserRole.create({
-                    user_id:UserResult.id,
-                    role_id:1
-                },{
-                    transaction:transaction
+                    user_id: UserResult.id,
+                    role_id: 1
+                }, {
+                    transaction: transaction
                 });
 
                 await transaction.commit();
 
                 return res.status(201).json({
-                    success:true,
-                    message:'New User was created Successfully'
+                    success: true,
+                    message: 'New User was created Successfully'
                 });
             } catch (e) {
 
                 await transaction.rollback();
                 console.log(e);
                 return res.status(500).json({
-                    success:false,
-                    message:'Something went wrong. Try again'
+                    success: false,
+                    message: 'Something went wrong. Try again'
                 });
             }
 
         } catch (e) {
-            console.log(e);
             return res.status(500).json({
-                success:false,
-                message:'Something went wrong. Try again'
+                success: false,
+                message: 'Something went wrong. Try again'
             });
         }
     } catch (e) {
         return res.status(500).json({
-            success:false,
-            message:'The Following request bodies are required. first_name, last_name, email_address, phone_number, password, confirm_password'
+            success: false,
+            message: 'The Following request bodies are required. first_name, last_name, email_address, phone_number, password, confirm_password'
         });
     }
 }
