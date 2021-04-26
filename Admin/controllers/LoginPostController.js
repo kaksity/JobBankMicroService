4ff83f2b80fdc3d1d60d2226bb6b4f1f39c8f664
coldflame <daudapona2@gmail.com>
2021-04-26 13:19:55 +0100

    Made Some Changes to the API

--- Admin/controllers/LoginPostController.js
+++ Admin/controllers/LoginPostController.js
@@ -4,89 +4,90 @@ const validator = require('validator')
 const GeneralFunctions = require('../functions/GeneralFunctions');
 const UserRole = require('../models/UserRole');
 
-module.exports = async (req,res)=>{
+module.exports = async(req, res) => {
     try {
         let EmailAddress = req.body.email_address;
         let Password = req.body.password;
 
-        if(validator.isEmpty(EmailAddress)){
+        if (validator.isEmpty(EmailAddress)) {
             return res.status(400).json({
-                success:false,
-                message:'Email Address is required'
+                success: false,
+                message: 'Email Address is required'
             });
-        } else if (validator.isEmpty(Password)){
+        } else if (validator.isEmpty(Password)) {
             return res.status(400).json({
-                success:false,
-                message:'Password is required'
+                success: false,
+                message: 'Password is required'
             });
-        } else if (Password.length < 8){
+        } else if (Password.length < 8) {
             return res.status(400).json({
-                success:false,
-                message:'Incorrect Login Credentials'
+                success: false,
+                message: 'Incorrect Login Credentials'
             });
         }
 
         EmailAddress = EmailAddress.toLowerCase();
-        
+
         try {
             let result = await UserInfo.findOne({
-                where:{
-                    email_address:EmailAddress
+                where: {
+                    email_address: EmailAddress
                 },
             });
-            
 
-            if(!result){
+
+            if (!result) {
                 return res.status(404).json({
-                    success:false,
-                    message:'Incorrect Login Credentials'
+                    success: false,
+                    message: 'Incorrect Login Credentials'
                 });
             }
 
-            const isPasswordValid = await bcrypt.compare(Password,result.password);
+            const isPasswordValid = await bcrypt.compare(Password, result.password);
 
-            if(isPasswordValid == false){
+            if (isPasswordValid == false) {
                 return res.status(404).json({
-                    success:false,
-                    message:'Incorrect Login Credentials'
-                }); 
+                    success: false,
+                    message: 'Incorrect Login Credentials'
+                });
             }
 
             const UserRoleResult = await UserRole.findOne({
-                where:{
-                    user_id:result.id
+                attributes: ['role_id'],
+                where: {
+                    user_id: result.id
                 }
             });
 
-            if(!UserRoleResult || UserRoleResult.role_id != 2){
+            if (!UserRoleResult || UserRoleResult.role_id != 2) {
                 return res.status(404).json({
-                    success:false,
-                    message:'Incorrect Login Credentials'
+                    success: false,
+                    message: 'Incorrect Login Credentials'
                 });
             }
 
             JWTData = {
-                id:result.id
+                id: result.id
             };
 
             return res.status(200).json({
-                success:true,
-                message:'Login was successful',
-                data:{
-                    token:GeneralFunctions.signToken(JWTData)
+                success: true,
+                message: 'Login was successful',
+                data: {
+                    token: GeneralFunctions.signToken(JWTData)
                 }
             });
         } catch (e) {
             return res.status(500).json({
-                success:false,
-                message:'Something went wrong. Try again'
-            });            
+                success: false,
+                message: 'Something went wrong. Try again'
+            });
         }
-        
+
     } catch (e) {
         return res.status(500).json({
-            success:false,
-            message:'The following request bodies are required email_address & password'
+            success: false,
+            message: 'The following request bodies are required email_address & password'
         })
     }
 }
--- Admin/middleware/VerifyAdmin.js
+++ Admin/middleware/VerifyAdmin.js
@@ -1,34 +1,35 @@
 const UserRole = require('../models/UserRole');
 
-module.exports = async(req,res,next)=>{
+module.exports = async(req, res, next) => {
     try {
-    
+
         let AdminID = req.admin.id;
 
-        if(!AdminID){
+        if (!AdminID) {
             return res.status(401).json({
-                message:'You must log in as Admin',
-                success:false,
+                message: 'You must log in as Admin',
+                success: false,
             });
         }
 
         let result = await UserRole.findOne({
-            where:{
-                user_id:AdminID
+            attributes: ['role_id'],
+            where: {
+                user_id: AdminID
             }
         });
 
-        if(!result || result.role_id != 2){
+        if (!result || result.role_id != 2) {
             return res.status(401).json({
-                message:'You must log in as Admin',
-                success:false,
+                message: 'You must log in as Admin',
+                success: false,
             });
         }
         next();
-    }catch (e) {
+    } catch (e) {
         return res.status(500).json({
-            success:false,
-            message:'Something went wrong. Try again'
-        });    
+            success: false,
+            message: 'Something went wrong. Try again'
+        });
     }
 }
